928058d99e6366678087189de6b3487e84730890
Peyush Gedela <gedela.peyush2022@vitstudent.ac.in>
2024-07-11 14:16:09 +0530

    chgs

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use("/api/doctor", docRoute);
 const path = require("path");
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dev")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dev", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 
