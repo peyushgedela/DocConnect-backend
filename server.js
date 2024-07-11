e4df762b689e334bd9bd2a4b2c058b99cd5f2f7c
Peyush Gedela <gedela.peyush2022@vitstudent.ac.in>
2024-07-11 14:13:57 +0530

    changes

--- server.js
+++ server.js
@@ -13,4 +13,13 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", docRoute);
 
+const path = require("path");
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dev")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dev", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Node running on ${port}`));
